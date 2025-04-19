234b03e935020ad034bbcd3db01c4a42f8e9c155
Ayo Phillips <ayophillips@onepipe.io>
2025-04-19 13:45:51 +0100

    fix: rename EC2 instance resource from web1 to web for consistency

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ data "aws_key_pair" "my_key_pair" {
 }
 
 # EC2 Instance
-resource "aws_instance" "web1" {
+resource "aws_instance" "web" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
